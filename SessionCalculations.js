287e84ac96bdf63c73db9609d627b1aeae68e4b8
Tiesthatbind <SethuThirumal00@gmail.com>
2020-01-18 22:44:13 -0800

    Add files via upload

--- SessionCalculations.js
+++ SessionCalculations.js
@@ -1,26 +1,37 @@
 // JavaScript 
 
-function [content] = caffeine(time, caffeineContent, durationBetween)
-halfLife = 5.7;
-tau = halfLife / log(2);
-n = floor(time / durationBetween);
-content = caffeineContent * exp(-time / tau) * (1 - exp(durationBetween * (n + 1) / tau)) / (1 - exp(durationBetween / tau));
-end
 
-function [content] = caffeineList(time, caffeineContent, durationBetween)
-content = zeros(1, length(time));
-for i = 1: length(time)
-content(i) = caffeine(time(i), caffeineContent, durationBetween);
-end
-end
+// This takes the age and weight and then provides tha caffine content to be plugged into the algorithm
 
-function optcaffeine(int weight, int age) {
+function caffinecontent(int weight, int age, int hypo) {
     int a = 2.72 * weight;
+    
     if (age < 16) {
         a = 0.75 * a;
     }
+    if (hype = 0) {
+        a = 0.5 * a
+    }
+    else if (hype = 1) { }
+    else if (hype = 2) {
+        a = 2* a
+    }
+}
+function  caffeine(time, caffeineContent, durationBetween){
+    halfLife = 5.7;
+    tau = halfLife / log(2);
+    n = floor(time / durationBetween);
+    content = caffeineContent * exp(-time / tau) * (1 - exp(durationBetween * (n + 1) / tau)) / (1 - exp(durationBetween / tau));
+  
 }
 
+function caffeineList(time, caffeineContent, durationBetween) {
+    content = zeros(1, length(time));
+    for i = 1: length(time)
+    content(i) = caffeine(time(i), caffeineContent, durationBetween);
+}
+
+// this function gives a schedule in print form for the user to follow + some transitions 
 function schedule(int optcaff, int incaff) {
     int b = incaff / optcaff;
     int c = optcaff / b;
